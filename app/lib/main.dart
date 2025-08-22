20be7d58e35700fbc56a3630d899abe29dc96847
th33raphat <theeraphatchueanokkhum@gmail.com>
2025-08-22 07:00:28 +0700

    feat: setup footer

--- app/lib/main.dart
+++ app/lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:app/components/Button.dart';
-import 'package:app/components/Lottery.dart';
+import 'package:app/components/Footer.dart';
 import 'package:flutter/material.dart';
 import 'components/Header.dart';
 
@@ -13,17 +13,21 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       home: Scaffold(
         appBar: const Header(),
-        body: ListView(
-          primary: false,
-          shrinkWrap: true,
-          children: const [
-            ButtonActions(
-              hasShadow: true,
-              key: Key("Continue Key"),
-              text: "ไปกันเลย",
-            ),
-          ],
+        body: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 36, vertical: 36),
+          child: ListView(
+            primary: false,
+            shrinkWrap: true,
+            children: const [
+              ButtonActions(
+                hasShadow: true,
+                key: Key("Continue Key"),
+                text: "ไปกันเลย",
+              ),
+            ],
+          ),
         ),
+        bottomNavigationBar: Footer(),
       ),
     );
   }
