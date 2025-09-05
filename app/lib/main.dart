f19fe627d20338867a48a3ad501f8510596002fb
th33raphat <theeraphatchueanokkhum@gmail.com>
2025-09-05 17:50:26 +0700

    feat: implement upload and tojwt

--- app/lib/main.dart
+++ app/lib/main.dart
@@ -1,23 +1,23 @@
-import 'dart:developer';
+// import 'dart:developer';
 
 import 'package:app/pages/cart.page.dart';
 import 'package:app/pages/home.page.dart';
 import 'package:app/pages/login.page.dart';
 import 'package:app/pages/lottery.page.dart';
-import 'package:app/components/Avatar.dart';
-import 'package:app/components/Button.dart';
-import 'package:app/components/Footer.dart';
-import 'package:app/components/Input.dart';
-import 'package:app/components/Lottery.dart';
-import 'package:app/pages/notfound.page.dart';
+// import 'package:app/components/Avatar.dart';
+// import 'package:app/components/Button.dart';
+// import 'package:app/components/Footer.dart';
+// import 'package:app/components/Input.dart';
+// import 'package:app/components/Lottery.dart';
+// import 'package:app/pages/notfound.page.dart';
 import 'package:app/pages/profile.page.dart';
 import 'package:app/pages/purchase.page.dart';
 import 'package:app/pages/register.page.dart';
 import 'package:flutter/material.dart';
 import 'style/theme.dart';
-import 'package:app/components/ActiveButton.dart';
-import 'package:app/components/DisabledButton.dart';
-import 'package:app/components/์Select_Number_Button.dart';
+// import 'package:app/components/ActiveButton.dart';
+// import 'package:app/components/DisabledButton.dart';
+// import 'package:app/components/์Select_Number_Button.dart';
 
 void main() => runApp(const MyApp());
 
