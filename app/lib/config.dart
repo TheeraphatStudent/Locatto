97e04b9f1ab3cf3bdf0cc380698241fd55ce3e1d
th33raphat <theeraphatchueanokkhum@gmail.com>
2025-09-27 16:19:23 +0700

    feat: change to use local database

--- app/lib/config.dart
+++ app/lib/config.dart
@@ -1,6 +1,6 @@
 class AppConfig {
-  // final String baseUrl = "10.0.2.2:3000";
-  final String baseUrl = "locatto-67775182631.europe-west1.run.app";
+  final String baseUrl = "10.0.2.2:3000";
+  // final String baseUrl = "locatto-67775182631.europe-west1.run.app";
   final String jwtSecret = "locatto_secret_key";
   final String tokenStorageName = "LottocatToken";
   final String userIdStorage = "user_id";
